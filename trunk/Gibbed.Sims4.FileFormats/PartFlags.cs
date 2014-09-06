ac0f7c484736d82cce4cabf86b79b9c5fc56a7b8
rick <rick@7e46140e-de0a-e411-98e1-0030489379b8>
2014-09-06 22:08:18 +0000

    - Renamed CASPartFile to PartFile. - Cleaned up implementation thanks to EA's custom content guide.

---
+++ trunk/Gibbed.Sims4.FileFormats/PartFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gibbed.Sims4.FileFormats
+{
+    [Flags]
+    public enum PartFlags : byte
+    {
+        DefaultForBodyType = 1 << 0,
+        DefaultThumbnailPart = 1 << 1,
+        AllowForRandom = 1 << 2,
+        ShowInUI = 1 << 3,
+        ShowInSimInfoPanel = 1 << 4,
+        ShowInCASDemo = 1 << 5,
+    }
+}
